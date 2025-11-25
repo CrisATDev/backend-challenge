b1f19308ac0ef30abe5349fcbee21c515e6cf4ca
Cristhian <christianandradetoledo@gmail.com>
2025-11-25 20:23:46 +0100

    Initial commit - configuración base del proyecto

---
+++ src/main/kotlin/com/devsoulify/devsoulifybackend/DevsoulifyBackendApplication.kt
@@ -0,0 +1,11 @@
+package com.devsoulify.devsoulifybackend
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class DevsoulifyBackendApplication
+
+fun main(args: Array<String>) {
+    runApplication<DevsoulifyBackendApplication>(*args)
+}
---
+++ src/test/kotlin/com/devsoulify/devsoulifybackend/DevsoulifyBackendApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.devsoulify.devsoulifybackend
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class DevsoulifyBackendApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
